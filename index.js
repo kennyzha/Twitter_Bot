727bdb746637fae32a80938b487565b00c594292
Kenny <Kenny>
2017-01-07 22:24:33 -0500

    generalized index.js so that you can just change the keyword var and also only print error msgs when neccessary

--- index.js
+++ index.js
@@ -4,37 +4,51 @@ var Twit = require('twit');
 var config = require('./config');
 
 var T = new Twit(config);
-start();
+
+// change keyword to the word that you would like to search for
+var keyword = "puppy";
+
+start(keyword);
 setInterval(start, 1000*60*10);
-function start(){
-	T.get('search/tweets', { q: 'giveaway since:2016-12-30', count: 100 }, followAndRetweet);
+
+function start(criteria){
+	
+	T.get('search/tweets', { q: criteria, count: 10 }, followAndRetweet);
 }
 
 function followAndRetweet(err, data, response){
 	var tweets = data.statuses;
-	for(var i = 0; i < tweets.length; i++){
+	if(tweets != null){
 
-		if(!tweets[i].retweeted){
-			console.log(tweets[i].user.name);
-			retweetID(tweets[i].id_str);
-			followUserId(tweets[i].user.screen_name);
-		}else{
-			console.log("Retweeted this already.");
+		for(var i = 0; i < tweets.length; i++){
+
+			if(!tweets[i].retweeted){
+				retweetID(tweets[i].id_str);
+				followUserId(tweets[i].user.screen_name);
+			}else{
+				console.log("Retweeted this already.");
+			}
 		}
 	}
-
+	else
+		console.log("No tweets were found with the keyword " + keyword);		
 }
 
 function retweetID(tweet_id){
 	T.post('statuses/retweet/:id', { id: tweet_id }, function (err, data, response) {
-  	console.log("Retweet error is " + err);
+  		if(err != null)
+  			console.log("Retweet error is " + err);
+  		else
+  			console.log("Retweeting id " + tweet_id);
 	})
 }
 
 function followUserId(screenName){
 	T.post('friendships/create', {screen_name: screenName}, function (err, data, response) {
-  	console.log("Following " + screenName);
-  	console.log("Follow user error is " + err);
+  	if(err != null)
+  		console.log("Follow user error. Error is " + err);
+  	else
+  		console.log("Following " + screenName);
 	})
 
 }
